ea3c792fb55f2e42f4fa4673349255f7d05fa1ff
Jens Astrup <jensaiden@gmail.com>
2017-02-11 20:15:55 -0500

    Additional tests added for Agreement

--- tests/test_account.py
+++ tests/test_account.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from unittest.mock import Mock, patch
 
+from pyEchosign.classes.agreement import Agreement
 from pyEchosign.classes.account import EchosignAccount
 
 
@@ -14,3 +15,39 @@ class TestAccount(TestCase):
         self.mock_get.return_value.ok = True
         e = EchosignAccount('a string')
         self.assertEqual(e.access_token, 'a string')
+
+    def test_get_agreements(self):
+        self.mock_get.return_value.ok = True
+        e = EchosignAccount('a string')
+        mock_response = Mock()
+        expected_dict = {
+            "userAgreementList": [
+                {
+                    "agreementId": "1",
+                    "displayDate": "date",
+                    "displayUserSetInfos": [
+                        {
+                            "displayUserSetMemberInfos": [
+                                {
+                                    "email": "",
+                                    "company": "",
+                                    "fullName": ""
+                                }
+                            ],
+                            "displayUserSetName": ""
+                        }
+                    ],
+                    "esign": False,
+                    "latestVersionId": "",
+                    "name": "Test Document",
+                    "status": "WAITING_FOR_SIGNATURE"
+                }
+            ]
+        }
+        mock_response.json.return_value = expected_dict
+
+        # Assign our mock response as the result of our patched function
+        self.mock_get.return_value = mock_response
+
+        agreements = e.get_agreements()
+        self.assertIsInstance(agreements[0], Agreement)

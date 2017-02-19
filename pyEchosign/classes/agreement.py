2be6e92ac1e769dcb081dc27e484799fb320d76a
jensastrup <jensaiden@gmail.com>
2017-02-19 11:53:25 -0500

    Agreement id changed to echosign_id

--- pyEchosign/classes/agreement.py
+++ pyEchosign/classes/agreement.py
@@ -18,7 +18,7 @@ class Agreement(object):
     Attributes:
         account (EchosignAccount): An instance of :class:`EchosignAccount <pyEchosign.classes.account.EchosignAccount>`. All Agreement actions will be conducted under this account.
         fully_retrieved (bool): Whether or not the agreement has all information retrieved, or if only the basic information was pulled (such as when getting all agreements instead of requesting the specific agreement)
-        id (str): The ID assigned to the agreement by Echosign, used to identify the agreement via the API
+        echosign_id (str): The ID assigned to the agreement by Echosign, used to identify the agreement via the API
         name (str): The name of the document as specified by the sender
         status (Agreement.Status): The current status of the document (OUT_FOR_SIGNATURE, SIGNED, APPROVED, etc)
     """
@@ -26,9 +26,9 @@ class Agreement(object):
     def __init__(self, account: 'eaccount.EchosignAccount', **kwargs):
         self.account = account
         self.fully_retrieved = kwargs.pop('fully_retrieved', None)
-        self.id = kwargs.pop('fully_retrieved', None)
-        self.name = kwargs.pop('fully_retrieved', None)
-        provided_status = kwargs.pop('fully_retrieved', None)
+        self.echosign_id = kwargs.pop('echosign_id', None)
+        self.name = kwargs.pop('name', None)
+        provided_status = kwargs.pop('provided_status', None)
         if provided_status is not None:
             self.status = self.Status[provided_status]
         else:
@@ -69,16 +69,17 @@ class Agreement(object):
 
     def cancel(self):
         """ Cancels the agreement on Echosign. Agreement will still be visible in the Manage page. """
-        url = self.account.api_access_point + endpoints.CANCEL_AGREEMENT + self.id + '/status'
+        url = self.account.api_access_point + endpoints.CANCEL_AGREEMENT + self.echosign_id + '/status'
         body = dict(value='CANCEL')
         r = requests.put(url, headers=get_headers(self.account.access_token), data=json.dumps(body))
 
         if response_success(r):
-            log.debug('Request to cancel agreement {} successful.'.format(self.id))
+            log.debug('Request to cancel agreement {} successful.'.format(self.echosign_id))
 
         else:
             try:
-                log.error('Error encountered cancelling agreement {}. Received message: {}'.format(self.id, r.content))
+                log.error('Error encountered cancelling agreement {}. Received message: {}'.format(self.echosign_id,
+                                                                                                   r.content))
             finally:
                 check_error(r)
 
@@ -88,14 +89,15 @@ class Agreement(object):
         Warnings:
             This action requires the 'agreement_retention' scope, which doesn't appear to be actually available via OAuth
         """
-        url = self.account.api_access_point + endpoints.DELETE_AGREEMENT + self.id
+        url = self.account.api_access_point + endpoints.DELETE_AGREEMENT + self.echosign_id
 
         r = requests.delete(url, headers=get_headers(self.account.access_token))
 
         if response_success(r):
-            log.debug('Request to delete agreement {} successful.'.format(self.id))
+            log.debug('Request to delete agreement {} successful.'.format(self.echosign_id))
         else:
             try:
-                log.error('Error encountered deleting agreement {}. Received message:{}'.format(self.id, r.content))
+                log.error('Error encountered deleting agreement {}. Received message:{}'.format(self.echosign_id,
+                                                                                                r.content))
             finally:
                 check_error(r)

7bef1da6552ec70778b27945140681067a4746bb
Jens Astrup <jensaiden@gmail.com>
2017-03-11 19:26:42 -0500

    Project restructuring to fix pip issues

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='pyEchosign',
-    version='0.1.0dev',
-    packages=['utils', 'classes', 'exceptions'],
+    version='0.1.1dev',
+    packages=['pyEchosign', 'pyEchosign.classes', 'pyEchosign.exceptions', 'pyEchosign.utils'],
     url='https://gitlab.com/jensastrup/pyEchosign',
     license='MIT',
     author='Jens Astrup',
     author_email='jensaiden@gmail.com',
     description='Connect to the Echosign API without constructing HTTP requests',
-    long_description=open('../README.rst').read(),
+    long_description=open('README.rst').read(),
     install_requires=['requests>=2.12.4, <3.0.0', 'arrow>=0.10.0, <1.0.0'],
     tests_require=['coverage', 'nose'],
     keywords='adobe echosign',

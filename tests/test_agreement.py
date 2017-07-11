9805f0b2074c4ef50b08b8b160057b6b48075b94
Jens Astrup <jensaiden@gmail.com>
2017-07-10 21:21:01 -0400

    Additional tests added for Agreements

--- tests/test_agreement.py
+++ tests/test_agreement.py
@@ -48,3 +48,61 @@ class TestAccount(TestCase):
 
         with self.assertRaises(PermissionError):
             agreement.cancel()
+
+    def test_cancel_agreement_500_raises_error(self):
+        """ Test that an invalid response due to an issue with the API, not the package, raises an Exception """
+        mock_response = Mock()
+
+        account = EchosignAccount('an invalid string')
+        account.api_access_point = 'http://echosign.com'
+
+        agreement = Agreement(account=account)
+        agreement.name = 'Test Agreement'
+        agreement.fully_retrieved = False
+        agreement.echosign_id = '123'
+        agreement.date = '2017-02-19T08:22:34-08:00'
+
+        mock_response.status_code = 500
+        # Assign our mock response as the result of our patched function
+        self.mock_put.return_value = mock_response
+
+        with self.assertRaises(ApiError):
+            agreement.cancel()
+
+    def test_delete_agreement_passes(self):
+        mock_response = Mock()
+
+        account = EchosignAccount('an invalid string')
+        account.api_access_point = 'http://echosign.com'
+
+        agreement = Agreement(account=account)
+        agreement.name = 'Test Agreement'
+        agreement.fully_retrieved = False
+        agreement.echosign_id = '123'
+        agreement.date = '2017-02-19T08:22:34-08:00'
+
+        mock_response.status_code = 200
+        # Assign our mock response as the result of our patched function
+        self.mock_put.return_value = mock_response
+
+        agreement.cancel()
+
+    def test_delete_agreement_401_raises_error(self):
+        mock_response = Mock()
+
+        account = EchosignAccount('an invalid string')
+        account.api_access_point = 'http://echosign.com'
+
+        agreement = Agreement(account=account)
+        agreement.name = 'Test Agreement'
+        agreement.fully_retrieved = False
+        agreement.echosign_id = '123'
+        agreement.date = '2017-02-19T08:22:34-08:00'
+
+        mock_response.status_code = 401
+        # Assign our mock response as the result of our patched function
+        self.mock_put.return_value = mock_response
+
+        with self.assertRaises(PermissionError):
+            agreement.cancel()
+

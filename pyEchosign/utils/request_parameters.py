e329496150c355baac34d10a968b874407ef3ccd
Jens Astrup <jensaiden@gmail.com>
2017-10-23 21:12:22 -0400

    Update documentation for get_headers()

--- pyEchosign/utils/request_parameters.py
+++ pyEchosign/utils/request_parameters.py
@@ -2,6 +2,7 @@ def get_headers(access_token, api_user_email=None, content_type='application/jso
     """
     Generates the headers for a request to the API - can specify which user the API call should be made under.
     Args:
+        content_type: What content type should be specified in the request headers. Defaults to application/json.
         access_token: The access token of the account making the request
         api_user_email: If the access token covers more than one user, enter the email of the user the call should be made under
 
